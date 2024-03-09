62d6790e4d2a2d4f6d3c0b32aee8ff7d355dbedc
vip03pandey <vipul03pandey@gmail.com>
2024-03-09 16:43:32 +0530

    hooks

---
+++ 02Hooks/App.jsx
@@ -0,0 +1,22 @@
+import React, { useState } from 'react'
+
+function App() {
+    const [count,setcount]=useState(5);
+    const handleAdd=()=>{
+        setcount(count=>count+1)
+    }
+    const handleRemove=()=>{
+        if(count>0){
+        setcount(count=>count-1)
+        }
+    }
+  return (
+    <div>
+      <h1>Counter :{count} </h1>
+      <button onClick={handleAdd}>Add +1 </button>
+      <button onClick={handleRemove}>Decrease -1</button>
+    </div>
+  )
+}
+
+export default App
