66242a20d59bdc31f099e105d8f90a60ea49b0a7
vip03pandey <vipul03pandey@gmail.com>
2024-03-09 21:32:52 +0530

    props

---
+++ 03Props/src/App.jsx
@@ -0,0 +1,13 @@
+import Crad from "./Crad"
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-3xl text-center mb-3' >Tailwind Test</h1>
+      <Crad username="vipulPandey" btnText="hello"/>
+    </>
+  )
+}
+
+export default App
